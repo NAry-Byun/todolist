a057bdaf478915c32d85df488669bfcae9ab10ed
NAry-Byun <153330377+NAry-Byun@users.noreply.github.com>
2024-02-14 23:05:08 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,44 @@
-
-let taskclick = document.getElementById("task-click");
-let taskitem = document.getElementById("task-item");
-let itemlist=[];
-let chklist=[];
-
-taskclick.addEventListener("click",puttask);
-
-function puttask() {
-
-    let item = taskitem.value;
-    let tasklist = {
-        id : Math.random().toString(36).substring(2, 18),
-        item : item,
-        isComplete: false
-    }
-
-    itemlist.push(tasklist)
-
-    render();
+body{
+    margin:0;
+    background-image: url("image/1.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.container{
+    background-color: bisque;
+    min-height: 100vh;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 }
 
-function render() {
+.task-tabs{
 
-    let putHtml='';
-    for(let i=0; i < itemlist.length; i++){
-
-        putHtml += `<div class="task-item${itemlist[i].isComplete}"><div>${itemlist[i].item}</div><div>
-        <button onclick="checkitem('${itemlist[i].id}')">On Going</button>
-        <button onclick="deleitem('${itemlist[i].id}')">Delete</button></div></div>`;
-    }
-
-    document.getElementById("putitem").innerHTML=putHtml;    
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px 0;
+    background-color: #f5f5f5;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    position: relative;
 }
 
-function checkitem(chkid) {
-
-    for(let i=0; i < itemlist.length; i++){
-        if(itemlist[i].id === chkid) {
-            itemlist[i].isComplete = !itemlist[i].isComplete;
-            chklist.push(itemlist[i]);
-            break;
-        }
-    }
-
-    render();
+.tesk-area{
+    border: 1px solid lightgray;
 }
 
-function deleitem(chkid) {
-console.log(chkid)
-
-    for(let i=0; i < itemlist.length; i++){
-        if(itemlist[i].id === chkid) {
-            itemlist.splice(i,1);
-            break;
-        }
-    }
-
-    render();
+#under-line{
+height: 4px;
+background-color: hotpink;
+position:absolute;
+width:100px;
+left: 0;
+top: 65px;
+padding:0px;
+}
+.task-item {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    margin: 10px 0; /* 패딩을 조절하여 간격을 만듭니다 */
+    background-color: #f5f5f5;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 }
